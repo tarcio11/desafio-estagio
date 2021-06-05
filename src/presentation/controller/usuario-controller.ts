a1ede37d0db3348bddd06fcf3701c63d643e877b
tarcio11 <tarcio_x@hotmail.com>
2021-06-05 17:47:58 -0300

    feat: certifique-se de UsuarioController chamar Usuario com os valores corretos

--- src/presentation/controller/usuario-controller.ts
+++ src/presentation/controller/usuario-controller.ts
@@ -1,14 +1,19 @@
+import { Usuario } from '../../entities/usecases'
 import { badRequest } from '../helpers'
 import { Controller, HttpResponse, Validation } from '../protocols'
 
 export class UsuarioController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly usuario: Usuario
+  ) {}
 
   async handle (request: UsuarioController.Request): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
+    await this.usuario.add(request)
     return null
   }
 }
