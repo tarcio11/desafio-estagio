f45154c1e912d91c3bc58cfa28774f27eb41df12
tarcio11 <tarcio_x@hotmail.com>
2021-06-05 12:54:21 -0300

    test: certifique-se de chamar hash com os valores corretos

---
+++ src/external/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,19 @@
+import { BcryptAdapter } from './bcrypt-adapter'
+
+import bcrypt from 'bcryptjs'
+
+const salt = 12
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('Bcrypt Adapter', () => {
+  describe('hash()', () => {
+    test('Deve chamar hash com os valores corretos', async () => {
+      const sut = makeSut()
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
+      await sut.hash('any_value')
+      expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    })
+  })
+})
