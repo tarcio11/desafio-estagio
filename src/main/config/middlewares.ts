bf79b50127afc2e7e7c4d54996093b7b07ba87c2
tarcio11 <tarcio_x@hotmail.com>
2021-06-05 18:23:01 -0300

    feat: certifique-se de retornar o tipo de conteúdo padrão json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
-import { bodyParser, cors } from '../middlewares'
+import { bodyParser, cors, contentType } from '../middlewares'
 
 import { Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
