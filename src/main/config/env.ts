86eaf92a1e2a625326d11965fd4cf4d480bd1bd7
tarcio11 <tarcio_x@hotmail.com>
2021-06-05 20:14:18 -0300

    refactor: mudar o nome da database

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/campeonato-ts-api',
+  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/desafio-estagio',
   port: process.env.PORT || 5000
 }
