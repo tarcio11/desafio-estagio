3a3c3349b35a91d5e6c1773c8586ec3ad971d14f
tarcio11 <tarcio_x@hotmail.com>
2021-06-05 13:38:52 -0300

    test: certifique-se de retorna um erro se EmailValidator retornar falso

---
+++ src/validation/email-validation.spec.ts
@@ -0,0 +1,41 @@
+import { EmailValidation } from './email-validation'
+import { EmailValidator } from '../presentation/protocols'
+import { InvalidParamError } from '../presentation/errors'
+
+import faker from 'faker'
+
+const field = faker.random.word()
+
+export class EmailValidatorSpy implements EmailValidator {
+  email: string
+  isEmailValid = true
+
+  isValid (email: string): boolean {
+    this.email = email
+    return this.isEmailValid
+  }
+}
+
+type SutTypes = {
+  sut: EmailValidation
+  emailValidatorSpy: EmailValidatorSpy
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorSpy = new EmailValidatorSpy()
+  const sut = new EmailValidation(field, emailValidatorSpy)
+  return {
+    sut,
+    emailValidatorSpy
+  }
+}
+
+describe('Email Validation', () => {
+  test('Deve retorna um erro se EmailValidator retornar falso', () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    emailValidatorSpy.isEmailValid = false
+    const email = faker.internet.email()
+    const error = sut.validate({ [field]: email })
+    expect(error).toEqual(new InvalidParamError(field))
+  })
+})
