ffe8ffdecb1bef3f1b7cd0471f4d5b799c1d40c4
tarcio11 <tarcio_x@hotmail.com>
2021-06-05 13:06:02 -0300

    feat: certifique-se de retorna um Usuario em caso de sucesso

--- src/external/db/mongodb/usuario/user-mongo-repository.ts
+++ src/external/db/mongodb/usuario/user-mongo-repository.ts
@@ -1,7 +1,13 @@
-import { CheckUserByEmailRepository } from '../../../../usecases/protocols'
+import { CheckUserByEmailRepository, AddUserRepository } from '../../../../usecases/protocols'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class UserMongoRepository implements CheckUserByEmailRepository {
+export class UserMongoRepository implements CheckUserByEmailRepository, AddUserRepository {
+  async add (data: AddUserRepository.Params): Promise<AddUserRepository.Response> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const result = accountCollection.insertOne(data)
+    return (await result).ops[0] !== null
+  }
+
   async checkByEmail (email: string): Promise<boolean> {
     const accountCollection = await MongoHelper.getCollection('users')
     const account = await accountCollection.findOne({
