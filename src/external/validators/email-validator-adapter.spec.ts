f6cc94da02117976d8977938c9002175b77f23ea
tarcio11 <tarcio_x@hotmail.com>
2021-06-05 16:34:47 -0300

    test: certifique-se de chamar o validator com email correto

---
+++ src/external/validators/email-validator-adapter.spec.ts
@@ -0,0 +1,24 @@
+import { EmailValidatorAdapter } from './email-validator-adapter'
+
+import validator from 'validator'
+import faker from 'faker'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
+
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
+describe('EmailValidatorAdapter', () => {
+  test('Deve chamar o validator com email correto', () => {
+    const sut = makeSut()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    const email = faker.internet.email()
+    sut.isValid(email)
+    expect(isEmailSpy).toHaveBeenCalledWith(email)
+  })
+})
