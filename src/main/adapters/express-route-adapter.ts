6e058eb4aef4c11c54664edcced16ab0ec3f3223
tarcio11 <tarcio_x@hotmail.com>
2021-06-05 19:11:57 -0300

    feat: adicionar ExpressRouteAdapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import { Controller } from '../../presentation/protocols'
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const request = {
+      ...(req.body || {})
+    }
+    const httpResponse = await controller.handle(request)
+    if (httpResponse.statusCode === 200) {
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
