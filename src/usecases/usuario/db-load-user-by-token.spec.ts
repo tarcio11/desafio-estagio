ee2db5a9cbbf25a755967450a6e13ccf5d54f135
tarcio11 <tarcio_x@hotmail.com>
2021-06-06 10:09:51 -0300

    test: certifique-se de chamar Decrypter com o token correto

---
+++ src/usecases/usuario/db-load-user-by-token.spec.ts
@@ -0,0 +1,42 @@
+import { DbLoadAccountByToken } from './db-load-user-by-token'
+import { Decrypter } from '../protocols'
+
+import faker from 'faker'
+
+class DecrypterSpy implements Decrypter {
+  token: string
+  response = faker.datatype.uuid()
+
+  async decrypt (token: string): Promise<string> {
+    this.token = token
+    return this.response
+  }
+}
+
+type SutTypes = {
+  sut: DbLoadAccountByToken
+  decrypterSpy: DecrypterSpy
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterSpy = new DecrypterSpy()
+  const sut = new DbLoadAccountByToken(decrypterSpy)
+  return {
+    sut,
+    decrypterSpy
+  }
+}
+
+let token: string
+
+describe('DbLoadAccountByToken caso de uso', () => {
+  beforeEach(() => {
+    token = faker.datatype.uuid()
+  })
+
+  test('Deve chamar Decrypter com o token correto', async () => {
+    const { sut, decrypterSpy } = makeSut()
+    await sut.load(token)
+    expect(decrypterSpy.token).toBe(token)
+  })
+})
