e8c97b383bdfbd2a76f2cf7e39367dd1c5e82bb3
tarcio11 <tarcio_x@hotmail.com>
2021-06-06 01:41:00 -0300

    test: certifique-se de LoginController chamar Authentication com os valores corretos

--- src/presentation/controller/login-controller.spec.ts
+++ src/presentation/controller/login-controller.spec.ts
@@ -2,6 +2,7 @@ import { LoginController } from './login-controller'
 import { Validation } from '../protocols'
 import { MissingParamError } from '../errors'
 import { badRequest } from '../helpers'
+import { Authentication } from '../../entities/usecases'
 
 import faker from 'faker'
 
@@ -15,6 +16,19 @@ export class ValidationSpy implements Validation {
   }
 }
 
+export class AuthenticationSpy implements Authentication {
+  params: Authentication.Params
+  response: Authentication.Result = {
+    nomeCompleto: faker.name.findName(),
+    tokenDeAcesso: faker.datatype.uuid()
+  }
+
+  async auth (params: Authentication.Params): Promise<Authentication.Result> {
+    this.params = params
+    return this.response
+  }
+}
+
 const mockAddAccountParams = (): LoginController.Request => ({
   email: faker.internet.email(),
   senha: faker.internet.password()
@@ -23,14 +37,17 @@ const mockAddAccountParams = (): LoginController.Request => ({
 type SutTypes = {
   sut
   validationSpy: ValidationSpy
+  authenticationSpy: AuthenticationSpy
 }
 
 const makeSut = (): SutTypes => {
   const validationSpy = new ValidationSpy()
-  const sut = new LoginController(validationSpy)
+  const authenticationSpy = new AuthenticationSpy()
+  const sut = new LoginController(validationSpy, authenticationSpy)
   return {
     sut,
-    validationSpy
+    validationSpy,
+    authenticationSpy
   }
 }
 
@@ -47,5 +64,15 @@ describe('Login Controller', () => {
     validationSpy.error = new MissingParamError(faker.random.word())
     const httpResponse = await sut.handle(mockAddAccountParams())
     expect(httpResponse).toEqual(badRequest(validationSpy.error))
+  })
+
+  test('Deve chamar Authentication com os valores corretos', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const request = mockAddAccountParams()
+    await sut.handle(request)
+    expect(authenticationSpy.params).toEqual({
+      email: request.email,
+      senha: request.senha
+    })
   })
 })
