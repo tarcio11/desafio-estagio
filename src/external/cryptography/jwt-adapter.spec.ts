8f2a29b6ca6b66391fa0be457a36a5c60e7538f2
tarcio11 <tarcio_x@hotmail.com>
2021-06-06 00:56:03 -0300

    test: certifique-se de retornar erro de sign se retornar erro

--- src/external/cryptography/jwt-adapter.spec.ts
+++ src/external/cryptography/jwt-adapter.spec.ts
@@ -27,5 +27,12 @@ describe('JWT Adapter', () => {
       const accessToken = await sut.encrypt('any_id')
       expect(accessToken).toBe('any_token')
     })
+
+    test('Deve retornar erro de sign se retornar erro', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+      const promise = sut.encrypt('any_id')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
