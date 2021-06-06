4a333d6c1950990ade3be11d7be8446c3743d177
tarcio11 <tarcio_x@hotmail.com>
2021-06-06 10:35:43 -0300

    feat: certifique-se de chamar verify com os valores corretos

--- src/external/cryptography/jwt-adapter.ts
+++ src/external/cryptography/jwt-adapter.ts
@@ -1,11 +1,16 @@
-import { Encrypter } from '../../usecases/protocols'
+import { Decrypter, Encrypter } from '../../usecases/protocols'
 
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (id: string): Promise<string> {
     return jwt.sign({ id }, this.secret)
+  }
+
+  async decrypt (token: string): Promise<string> {
+    jwt.verify(token, this.secret)
+    return null
   }
 }
