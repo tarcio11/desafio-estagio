d05c4bcbca2845d6e504d66b7574b0ffcc10ccaf
tarcio11 <tarcio_x@hotmail.com>
2021-06-05 23:36:35 -0300

    refactor: mudar o coverage para nao pegar as interfaces

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/**/protocols/**',
+    '!<rootDir>/src/entities/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
