a4d5ea8428dfa70c322172e0f3c815ff3e32c09b
tarcio11 <tarcio_x@hotmail.com>
2021-06-06 15:54:50 -0300

    test: certifique-se de chamar ValidationComposite com todas as validações

---
+++ src/main/factories/controllers/register-imovel-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+import { makeRegisterImovelValidation } from './register-imovel-validation-factory'
+import { Validation } from '../../../presentation/protocols'
+import { RequiredFieldValidation, ValidationComposite } from '../../../validation'
+
+jest.mock('../../../validation/validation-composite')
+
+describe('LoginValidation Factory', () => {
+  test('Deve chamar ValidationComposite com todas as validações', () => {
+    makeRegisterImovelValidation()
+    const validations: Validation[] = []
+    for (const field of ['userId', 'cep', 'numero', 'complemento', 'valor_do_aluguel_em_reais', 'quantidade_de_quartos', 'disponivel']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
