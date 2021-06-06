3700b913a9a9645e4544a5db94c0cb30a806138d
tarcio11 <tarcio_x@hotmail.com>
2021-06-06 00:38:45 -0300

    feat: certifique-se de chamar compare com os valores corretos

--- src/external/cryptography/bcrypt-adapter.ts
+++ src/external/cryptography/bcrypt-adapter.ts
@@ -1,11 +1,16 @@
-import { Hasher } from '../../usecases/protocols'
+import { HashComparer, Hasher } from '../../usecases/protocols'
 
 import bcrypt from 'bcryptjs'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   constructor (private readonly salt: number) {}
 
   async hash (plaintext: string): Promise<string> {
     return await bcrypt.hash(plaintext, this.salt)
+  }
+
+  async compare (senha: string, hashedSenha: string): Promise<boolean> {
+    await bcrypt.compare(senha, hashedSenha)
+    return null
   }
 }
