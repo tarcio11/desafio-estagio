9ac66f24381f2570e6304bc6d4fcef1969a5e896
tarcio11 <tarcio_x@hotmail.com>
2021-06-05 23:41:20 -0300

    feat: adicionar Athentication caso de usos

---
+++ src/entities/usecases/authentication.ts
@@ -0,0 +1,15 @@
+export interface Authentication {
+  auth: (authenticationParams: Authentication.Params) => Promise<Authentication.Result>
+}
+
+export namespace Authentication {
+  export type Params = {
+    email: string
+    senha: string
+  }
+
+  export type Result = {
+    tokenDeAcesso: string
+    nome: string
+  }
+}
