05563f6f66918f1c00b360a304f375578a0b0433
tarcio11 <tarcio_x@hotmail.com>
2021-06-06 15:17:22 -0300

    test: certifique-se de chamar Validation com os valores corretos

---
+++ src/presentation/controller/register-imovel-controller.spec.ts
@@ -0,0 +1,47 @@
+import { RegisterImovelController } from './register-imovel-controller'
+import { Validation } from '../protocols'
+
+import faker from 'faker'
+
+export class ValidationSpy implements Validation {
+  input: any
+  error: Error = null
+
+  validate (input: any): Error {
+    this.input = input
+    return this.error
+  }
+}
+
+const mockRegisterImovelParams = (): RegisterImovelController.Request => ({
+  userId: faker.datatype.uuid(),
+  cep: faker.address.zipCode(),
+  complemento: faker.address.cityPrefix(),
+  numero: Number(faker.finance.amount(1, 3000, null)),
+  quantidade_de_quartos: faker.datatype.number(4),
+  valor_do_aluguel_em_reais: faker.finance.amount(600, 1500),
+  disponivel: faker.datatype.boolean()
+})
+
+type SutTypes = {
+  sut
+  validationSpy: ValidationSpy
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
+  const sut = new RegisterImovelController(validationSpy)
+  return {
+    sut,
+    validationSpy
+  }
+}
+
+describe('RegisterImovel Controller', () => {
+  test('Deve chamar Validation com os valores corretos', async () => {
+    const { sut, validationSpy } = makeSut()
+    const request = mockRegisterImovelParams()
+    await sut.handle(request)
+    expect(validationSpy.input).toEqual(request)
+  })
+})
