281842d900f9c4a25d3338f5e90088f417d3c517
tarcio11 <tarcio_x@hotmail.com>
2021-06-07 13:37:36 -0300

    feat: certifique-se de retornar erro 500 se UpdateImovel arremessar um erro

--- src/presentation/controller/update-imovel-controller.ts
+++ src/presentation/controller/update-imovel-controller.ts
@@ -1,17 +1,21 @@
 import { UpdateImovel } from '../../entities/usecases/imoveis'
 import { InvalidParamError } from '../errors'
-import { forbidden } from '../helpers'
+import { forbidden, serverError } from '../helpers'
 import { Controller, HttpResponse } from '../protocols'
 
 export class UpdateImovelController implements Controller {
   constructor (private readonly updateImove: UpdateImovel) {}
 
   async handle (request: UpdateImovelController.Request): Promise<HttpResponse> {
-    const imovelModel = await this.updateImove.update(request)
-    if (!imovelModel) {
-      return forbidden(new InvalidParamError('imovelId'))
+    try {
+      const imovelModel = await this.updateImove.update(request)
+      if (!imovelModel) {
+        return forbidden(new InvalidParamError('imovelId'))
+      }
+      return null
+    } catch (error) {
+      return serverError()
     }
-    return null
   }
 }
 
