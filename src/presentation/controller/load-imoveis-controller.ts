ebbcecbbb9e3cc58b46da9073f0abc1b0625ef46
tarcio11 <tarcio_x@hotmail.com>
2021-06-07 15:44:34 -0300

    feat: certifique-se de  retornar 200 em caso de sucesso

--- src/presentation/controller/load-imoveis-controller.ts
+++ src/presentation/controller/load-imoveis-controller.ts
@@ -1,10 +1,12 @@
 import { LoadImoveis } from '../../entities/usecases/imoveis'
+import { ok } from '../helpers'
 import { Controller, HttpResponse } from '../protocols'
 
 export class LoadImoveisController implements Controller {
   constructor (private readonly loadImoveis: LoadImoveis) {}
 
   async handle (): Promise<HttpResponse> {
-    return null
+    const imoveisModel = await this.loadImoveis.load()
+    return ok(imoveisModel)
   }
 }
