d0d21e8a168dc990f74db58355e2b95eaf4f94f4
tarcio11 <tarcio_x@hotmail.com>
2021-06-07 17:24:56 -0300

    feat: certifique-se de retornar erro 500 se LoadImovelById arremessar um erro

--- src/presentation/controller/load-imovel-by-id-controller.ts
+++ src/presentation/controller/load-imovel-by-id-controller.ts
@@ -1,14 +1,18 @@
 import { Controller, HttpResponse } from '../protocols'
 import { LoadImovelById } from '../../entities/usecases/imoveis'
-import { ok, unauthorized } from '../helpers'
+import { ok, serverError, unauthorized } from '../helpers'
 
 export class LoadImovelByIdController implements Controller {
   constructor (private readonly loadImovelById: LoadImovelById) {}
 
   async handle (request: LoadImovelByIdController.Request): Promise<HttpResponse> {
-    const { imovelId } = request
-    const imovelModel = await this.loadImovelById.load(imovelId)
-    return imovelModel ? ok(imovelModel) : unauthorized()
+    try {
+      const { imovelId } = request
+      const imovelModel = await this.loadImovelById.load(imovelId)
+      return imovelModel ? ok(imovelModel) : unauthorized()
+    } catch (error) {
+      return serverError()
+    }
   }
 }
 
