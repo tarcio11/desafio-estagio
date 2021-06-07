84099a528716899c8a680a494a20cee0f3109884
tarcio11 <tarcio_x@hotmail.com>
2021-06-07 17:37:32 -0300

    feat: certifique-se de retornar 403 em caso de ausência de tokenDeAcesso

--- src/main/routes/imovel-routes.ts
+++ src/main/routes/imovel-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { adaptMiddleware, adaptRoute } from '../adapters'
-import { makeLoadImoveisController, makeRegisterImovelController, makeUpdateImovelController } from '../factories/controllers'
+import { makeLoadImoveisController, makeLoadImovelByIdController, makeRegisterImovelController, makeUpdateImovelController } from '../factories/controllers'
 import { makeAuthMiddleware } from '../factories/middlewares'
 
 export default (router: Router): void => {
   router.get('/imoveis', adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeLoadImoveisController()))
+  router.get('/imoveis/:imovelId', adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeLoadImovelByIdController()))
   router.put('/imoveis/:imovelId', adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeUpdateImovelController()))
   router.post('/imoveis', adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeRegisterImovelController()))
 }
