3665f44f53631054e7a1151645ac7b2ec3f192c6
tarcio11 <tarcio_x@hotmail.com>
2021-06-07 17:14:18 -0300

    test: certifique-se de chamar LoadImovelById com o id correto

---
+++ src/presentation/controller/load-imovel-by-id-controller.spec.ts
@@ -0,0 +1,50 @@
+import { LoadImovelByIdController } from './load-imovel-by-id-controller'
+import { LoadImovelById } from '../../entities/usecases/imoveis'
+
+import faker from 'faker'
+
+const mockRequest = (): LoadImovelByIdController.Request => ({
+  imovelId: faker.random.uuid()
+})
+
+class LoadImovelByIdSpy implements LoadImovelById {
+  imovelId: string
+  response: LoadImovelById.Result = {
+    id: faker.datatype.uuid(),
+    userId: faker.datatype.uuid(),
+    cep: faker.address.zipCode(),
+    complemento: faker.address.cityPrefix(),
+    numero: Number(faker.finance.amount(1, 3000, null)),
+    quantidade_de_quartos: faker.datatype.number(4),
+    valor_do_aluguel_em_reais: faker.finance.amount(600, 1500),
+    disponivel: faker.datatype.boolean()
+  }
+
+  async load (imovelId: string): Promise<LoadImovelById.Result> {
+    this.imovelId = imovelId
+    return this.response
+  }
+}
+
+type SutTypes = {
+  sut: LoadImovelByIdController
+  loadImovelByIdSpy: LoadImovelByIdSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadImovelByIdSpy = new LoadImovelByIdSpy()
+  const sut = new LoadImovelByIdController(loadImovelByIdSpy)
+  return {
+    sut,
+    loadImovelByIdSpy
+  }
+}
+
+describe('LoadImovels Controller', () => {
+  test('Deve chamar LoadImovelById com o id correto', async () => {
+    const { sut, loadImovelByIdSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(loadImovelByIdSpy.imovelId).toBe(request.imovelId)
+  })
+})
