1bac97e5b489a194469f6d9bae207f8077b6694d
tarcio11 <tarcio_x@hotmail.com>
2021-06-07 13:35:15 -0300

    feat: certifique-se de retornar 403 se UpdateImovel retornar nulo

--- src/presentation/controller/update-imovel-controller.ts
+++ src/presentation/controller/update-imovel-controller.ts
@@ -1,11 +1,16 @@
 import { UpdateImovel } from '../../entities/usecases/imoveis'
+import { InvalidParamError } from '../errors'
+import { forbidden } from '../helpers'
 import { Controller, HttpResponse } from '../protocols'
 
 export class UpdateImovelController implements Controller {
   constructor (private readonly updateImove: UpdateImovel) {}
 
   async handle (request: UpdateImovelController.Request): Promise<HttpResponse> {
-    await this.updateImove.update(request)
+    const imovelModel = await this.updateImove.update(request)
+    if (!imovelModel) {
+      return forbidden(new InvalidParamError('imovelId'))
+    }
     return null
   }
 }
