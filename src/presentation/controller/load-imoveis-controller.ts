a112d92d33e2780c77a97f7b6ea6877edf6d3312
tarcio11 <tarcio_x@hotmail.com>
2021-06-07 15:50:47 -0300

    feat: certifique-se de retornar erro 500 se LoadImoveis arremessar um erro

--- src/presentation/controller/load-imoveis-controller.ts
+++ src/presentation/controller/load-imoveis-controller.ts
@@ -1,12 +1,16 @@
 import { LoadImoveis } from '../../entities/usecases/imoveis'
-import { noContent, ok } from '../helpers'
+import { noContent, ok, serverError } from '../helpers'
 import { Controller, HttpResponse } from '../protocols'
 
 export class LoadImoveisController implements Controller {
   constructor (private readonly loadImoveis: LoadImoveis) {}
 
   async handle (): Promise<HttpResponse> {
-    const imoveisModel = await this.loadImoveis.load()
-    return imoveisModel.length ? ok(imoveisModel) : noContent()
+    try {
+      const imoveisModel = await this.loadImoveis.load()
+      return imoveisModel.length ? ok(imoveisModel) : noContent()
+    } catch (error) {
+      return serverError()
+    }
   }
 }
