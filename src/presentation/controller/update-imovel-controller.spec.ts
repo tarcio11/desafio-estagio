073b4f30ce0301d3e5e7b8f4ece494342d5b328d
tarcio11 <tarcio_x@hotmail.com>
2021-06-07 13:26:11 -0300

    test: certifique-se de chamar UpdateImovel com os valores corretos

---
+++ src/presentation/controller/update-imovel-controller.spec.ts
@@ -0,0 +1,56 @@
+import { UpdateImovelController } from './update-imovel-controller'
+import { UpdateImovel } from '../../entities/usecases/imoveis'
+
+import faker from 'faker'
+
+export class UpdateImovelSpy implements UpdateImovel {
+  params: UpdateImovel.Params
+  response: UpdateImovel.Response = {
+    id: faker.datatype.uuid(),
+    userId: faker.datatype.uuid(),
+    cep: faker.address.zipCode(),
+    complemento: faker.address.cityPrefix(),
+    numero: Number(faker.finance.amount(1, 3000, null)),
+    quantidade_de_quartos: faker.datatype.number(4),
+    valor_do_aluguel_em_reais: faker.finance.amount(600, 1500),
+    disponivel: faker.datatype.boolean()
+  }
+
+  async update (params: UpdateImovel.Params): Promise<UpdateImovel.Response> {
+    this.params = params
+    return this.response
+  }
+}
+
+const mockUpdateImovelParams = (): UpdateImovelController.Request => ({
+  imovelId: faker.datatype.uuid(),
+  userId: faker.datatype.uuid(),
+  data: {
+    cep: faker.address.zipCode(),
+    complemento: faker.address.cityPrefix(),
+    numero: Number(faker.finance.amount(1, 3000, null))
+  }
+})
+
+type SutTypes = {
+  sut: UpdateImovelController
+  updateImovelSpy: UpdateImovelSpy
+}
+
+const makeSut = (): SutTypes => {
+  const updateImovelSpy = new UpdateImovelSpy()
+  const sut = new UpdateImovelController(updateImovelSpy)
+  return {
+    sut,
+    updateImovelSpy
+  }
+}
+
+describe('RegisterImovel Controller', () => {
+  test('Deve chamar UpdateImovel com os valores corretos', async () => {
+    const { sut, updateImovelSpy } = makeSut()
+    const updateImovelParams = mockUpdateImovelParams()
+    await sut.handle(updateImovelParams)
+    expect(updateImovelSpy.params).toEqual(updateImovelParams)
+  })
+})
